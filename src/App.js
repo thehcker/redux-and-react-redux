cff16bf3f92510d3a37505651ab2ca66b9d0b179
ISAAC NYAKOI <isaacnyakoi1@gmail.com>
2020-02-25 19:05:43 +0300

    Add Redux and React Redux project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+
+//action creator
+const addItem = item => {
+  return {
+    type: "ADD_ITEM",
+    item: item
+  };
+};
+
+//reducer
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return [...state, action.item];
+    default:
+      return state;
+  }
+};
+
+var store = createStore(reducer);
+
+console.log(store.dispatch(addItem("a")));
+console.log(store.getState());
+console.log(store.dispatch(addItem("b")));
+console.log(store.getState());
+console.log(store.dispatch(addItem("c")));
+console.log(store.getState());
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World</h1>
     </div>
   );
 }
+
 
 export default App;
